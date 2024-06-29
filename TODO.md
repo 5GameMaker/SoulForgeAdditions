78b4f8588be6f3439f6274dc19a81ad415315b77
buj <buj@localhost>
2024-06-29 20:56:34 +0700

    Added tooltips

--- TODO.md
+++ TODO.md
@@ -3,7 +3,9 @@
 Things to be added in future (will be marked as "solved" if added in SoulForge)
 
 - [x] AppleSkin
-- [ ] Abilities tooltips
+- [x] Abilities tooltips
 - [ ] Abilities menu slot highlight + tooltips
 - [ ] Wiki: Soul reset
 - [ ] Wiki: Essences
+- [ ] Replace mod jars with gradle deps
+- [ ] Fix magic slot bug
--- src/main/java/buj/soulforgeadditions/mixin/SoulScreenMixin.java
+++ src/main/java/buj/soulforgeadditions/mixin/SoulScreenMixin.java
@@ -1,4 +1,26 @@
 package buj.soulforgeadditions.mixin;
 
+import com.pulsar.soulforge.ability.AbilityBase;
+import com.pulsar.soulforge.client.ui.SoulScreen;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.text.Texts;
+import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+
+@Mixin(SoulScreen.class)
 public class SoulScreenMixin {
+    @Redirect(method = "updateWidgets", at = @At(value = "INVOKE", target = "Lcom/pulsar/soulforge/ability/AbilityBase;getLocalizedText()Lnet/minecraft/text/Text;", shift = At.Shift.BY), require = 1)
+    public Text overrideLocalizedText(AbilityBase instance) {
+        return Texts.join(List.of(
+                instance.getLocalizedText(),
+                Text.translatable(
+                        "ability." + instance.getID().getPath() + ".summary"
+                ).setStyle(Style.EMPTY.withColor(Formatting.GRAY))
+        ), Text.of("\n\n"));
+    }
 }
